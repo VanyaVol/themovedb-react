a453c5c26d6b8f52b8b5bc14e56dda165678f9af
VanyaVol <vanya.streetworkout.sw.vol@gmail.com>
2022-02-12 17:46:51 +0200

    updated project TMDB

--- src/App.js
+++ src/App.js
@@ -23,6 +23,7 @@ function App() {
                     <Route path={"*"} element={<NotFoundPage/>}/>
                 </Route>
             </Routes>
+            <div>{}</div>
         </>
     );
 }
