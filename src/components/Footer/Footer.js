a8f035be8a42d8ff5b37e35df6f5988cc34dcdd4
VanyaVol <vanya.streetworkout.sw.vol@gmail.com>
2022-02-12 22:41:16 +0200

    updated project TMDB

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
     return (
         <footer className={css.footerBlock}>
             <div className={css.footer}>
-                <div className={css.year}>{new Date().getFullYear()}</div>
+                <div className={css.year}>{'Copyright © ' + new Date().getFullYear() + ' okten school project'}</div>
             </div>
         </footer>
     );
